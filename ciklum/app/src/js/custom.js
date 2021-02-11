37503915cdd2242bc12d123fd23f24e1a9613931
Vladymyr Radionov <radionovdon@gmail.com>
2021-02-11 23:47:04 +0200

    drawEvs1

--- ciklum/app/src/js/custom.js
+++ ciklum/app/src/js/custom.js
@@ -44,8 +44,30 @@ function addToStore() {
 //get from localStorage
 function getFromStore() {
     for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const value = localStorage.getItem(key);
-        console.log(JSON.parse(value));
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+        console.log(JSON.parse(value))
+        drawEvent(JSON.parse(value));
+    }
+}
+
+//draw the event
+function drawEvent(obj) {
+    const trId = document.getElementById(obj.time);
+    if (obj.days === 'Monday') {
+        trId.children[1].innerHTML = obj.name + '<button type="button" class="btn btn-outline-light border-0">x</button>';
+        trId.children[1].classList.add('bg-success');
+    } else if (obj.days === 'Tuesday') {
+        trId.children[2].innerHTML = obj.name + '<button type="button" class="btn btn-outline-light border-0">x</button>';
+        trId.children[2].classList.add('bg-success');
+    } else if (obj.days === 'Wednesday') {
+        trId.children[3].innerHTML = obj.name + '<button type="button" class="btn btn-outline-light border-0">x</button>';
+        trId.children[3].classList.add('bg-success');
+    } else if (obj.days === 'Thursday') {
+        trId.children[4].innerHTML = obj.name + '<button type="button" class="btn btn-outline-light border-0">x</button>';
+        trId.children[4].classList.add('bg-success');
+    } else if (obj.days === 'Friday') {
+        trId.children[5].innerHTML = obj.name + '<button type="button" class="btn btn-outline-light border-0">x</button>';
+        trId.children[5].classList.add('bg-success');
     }
 }
