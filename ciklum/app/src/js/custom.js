89ff6d6d8379cb3d7a4f871e116de069fc4efa23
Vladymyr Radionov <radionovdon@gmail.com>
2021-02-16 23:42:23 +0200

    mems

--- ciklum/app/src/js/custom.js
+++ ciklum/app/src/js/custom.js
@@ -3,35 +3,29 @@
 const buts = document.querySelectorAll('.dropdown-item'); //get all dropdown items
 const form = document.getElementById('create-event-form'); //get form
 const submit = document.getElementById('submit'); //get submit button
+const alMesBlock = document.getElementById('alertMes'); //get alert block
+const trs = document.querySelectorAll('.main-tr');
 const evName = document.getElementById('ev-name'); //get empty span in modal
 const remEv = document.getElementById('rem-event'); //get button to remove event
+
+
+
+let arrEVs = [];
 
 //get data from localStorage after load page
 self.onload = function () {
     getFromStore();
     // localStorage.clear();
-    console.log(JSON.parse(JSON.stringify(localStorage)))
+    checkMember('All members');
 }
 
-//change text in dropdown main button by choosing some inner item
-buts.forEach((item) => {
-    item.addEventListener('click', function () {
-        this.closest('div').firstElementChild.innerText = this.innerText;
-    })
-});
-
 //submit form by clicking submit button
-submit.addEventListener('click', function (e) {
-    e.preventDefault()
-    window.location.replace("../../build/index.html");
-    // if (!slot) {
+submit ? submit.addEventListener('click', function (e) {
+    e.preventDefault();
+    addToStore();
+}) : null;
 
-    // } else {
-        addToStore();
-    // }
-});
-
-// add to localStorage
+// add to localStorage and store data
 function addToStore() {
     let ev = {
         id: Date.now()
@@ -39,48 +33,98 @@ function addToStore() {
     for (let i = 0; i < form.elements.length - 1; i++) {
         let it = form.elements.item(i);
         ev[it.name] = it.value;
+        if (arrEVs.length) {
+            arrEVs.map(l => {
+                if (l.days === form.elements.item(2).value) {
+                    if (l.time === form.elements.item(3).value) {
+                        alMesBlock.style.display = 'block';
+                        throw new Error(); //end script
+                    }
+                }
+            })
+        }
     }
-    if (typeof (Storage) !== "undefined") {
-        self.localStorage.setItem(ev.id, JSON.stringify(ev));
-    } else {
-        alert('Sorry no storage support');
-    }
+    arrEVs.push(ev);
+    self.localStorage.setItem(ev.id, JSON.stringify(ev));
+    window.location.replace("./calendar.html");
 }
 
-//get from localStorage
+//get from localStorage, but actually from server
 function getFromStore() {
     for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i);
         let value = localStorage.getItem(key);
-        drawEvent(JSON.parse(value));
+        // drawEvent(JSON.parse(value));
+        arrEVs.push(JSON.parse(value));
     }
 }
 
 //draw the event
-function drawEvent(obj) {
-    const trId = document.getElementById(obj.time);
-    if (obj.days === 'Monday') {
-        trId.children[1].innerHTML = obj.name + `<button type="button" class="btn btn-outline-light border-0" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})">x</button>`;
-        trId.children[1].classList.add('bg-success');
-    } else if (obj.days === 'Tuesday') {
-        trId.children[2].innerHTML = obj.name + `<button type="button" class="btn btn-outline-light border-0" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})">x</button>`;
-        trId.children[2].classList.add('bg-success');
-    } else if (obj.days === 'Wednesday') {
-        trId.children[3].innerHTML = obj.name + `<button type="button" class="btn btn-outline-light border-0" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})">x</button>`;
-        trId.children[3].classList.add('bg-success');
-    } else if (obj.days === 'Thursday') {
-        trId.children[4].innerHTML = obj.name + `<button type="button" class="btn btn-outline-light border-0" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})">x</button>`;
-        trId.children[4].classList.add('bg-success');
-    } else if (obj.days === 'Friday') {
-        trId.children[5].innerHTML = obj.name + `<button type="button" class="btn btn-outline-light border-0" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})">x</button>`;
-        trId.children[5].classList.add('bg-success');
+function drawEvent(arr) {
+    arr.forEach(obj => {
+        const trId = document.getElementById(obj.time);
+        if (obj.days === 'Monday') {
+            trId.children[1].innerHTML = obj.name + `<span class="text-light fw-bold" style="cursor: pointer; float: right;" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})"> &times;</span>`;
+            trId.children[1].classList.add('bg-success');
+        } else if (obj.days === 'Tuesday') {
+            trId.children[2].innerHTML = obj.name + `<span class="text-light fw-bold" style="cursor: pointer; float: right;" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})"> &times;</span>`;
+            trId.children[2].classList.add('bg-success');
+        } else if (obj.days === 'Wednesday') {
+            trId.children[3].innerHTML = obj.name + `<span class="text-light fw-bold" style="cursor: pointer; float: right;" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})"> &times;</span>`;
+            trId.children[3].classList.add('bg-success');
+        } else if (obj.days === 'Thursday') {
+            trId.children[4].innerHTML = obj.name + `<span class="text-light fw-bold" style="cursor: pointer; float: right;" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})"> &times;</span>`;
+            trId.children[4].classList.add('bg-success');
+        } else if (obj.days === 'Friday') {
+            trId.children[5].innerHTML = obj.name + `<span class="text-light fw-bold" style="cursor: pointer; float: right;" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})"> &times;</span>`;
+            trId.children[5].classList.add('bg-success');
+        }
+    })
+}
+
+//check member name to display events
+function checkMember(name) {
+    let newArr = [];
+
+    name !== 'All members' ?
+        arrEVs.map((w) => {
+            for (let value of Object.values(w)) {
+                if (value === name) {
+                    newArr.push(w);
+                }
+            }
+        }) : newArr = [...arrEVs];
+    clearEvents();
+    drawEvent(newArr);
+    console.log(newArr);
+}
+
+//change text in dropdown main button by choosing some inner item
+buts.forEach((item) => {
+    item.addEventListener('click', function () {
+        this.closest('div').firstElementChild.innerText = this.innerText;
+        checkMember(this.innerText);
+    })
+});
+
+//clear every tr before draw new html events
+function clearEvents() {
+    for (let h = 0; h < trs.length; h++) {
+        for (let g = 1; g < trs[h].children.length; g++) {
+            trs[h].children[g].innerText = '';
+            trs[h].children[g].classList.remove('bg-success');
+        }
     }
 }
 
 //show modal to remove event from calendar
 function showModal(id) {
     let remObj = JSON.parse(localStorage.getItem(id));
-    evName.innerHTML = '"' + remObj.name + '"';
+    if (!!remObj.name) { //check if event has no name
+        evName.innerHTML = '"' + remObj.name + '"';
+    } else {
+        evName.innerHTML = 'unnamed';
+    }
 
     //remove event by clicking yes in modal
     remEv.addEventListener('click', function () {
@@ -88,3 +132,162 @@ function showModal(id) {
         location.reload();
     })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //get data from localStorage after load page
+// self.onload = function () {
+//     getFromStore();
+//     // localStorage.clear();
+//     console.log(arrEVs);
+// }
+
+// //change text in dropdown main button by choosing some inner item
+// buts.forEach((item) => {
+//     item.addEventListener('click', function () {
+//         this.closest('div').firstElementChild.innerText = this.innerText;
+//     })
+// });
+
+// //submit form by clicking submit button
+// submit.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     addToStore();
+//     // window.location.replace("../../build/index.html");
+//     // for (let k = 1; k < trAll.length; k++) {
+//     //     let ttt = trAll[k].id
+
+//     //     if (ttt.some('11:00')) {
+//     //         console.log(ttt)
+//     //     } else {
+
+//     //     }
+//     // }
+
+// });
+
+// // add to localStorage
+// function addToStore() {
+//     let ev = {
+//         id: Date.now()
+//     };
+//     for (let i = 0; i < form.elements.length - 1; i++) {
+//         let it = form.elements.item(i);
+//         ev[it.name] = it.value;
+//     }
+
+
+
+//     arrEVs.map((it, l) => {
+//         console.log(it.time, it.days)
+//     })
+
+
+
+
+//     // if (typeof (Storage) !== "undefined") {
+//     self.localStorage.setItem(ev.id, JSON.stringify(ev));
+//     // } else {
+//     //     alert('Sorry no storage support');
+//     // }
+// }
+
+// //get from localStorage
+// function getFromStore() {
+//     for (let i = 0; i < localStorage.length; i++) {
+//         let key = localStorage.key(i);
+//         let value = localStorage.getItem(key);
+//         drawEvent(JSON.parse(value));
+//         arrEVs.push(JSON.parse(value));
+//     }
+// }
+
+// //draw the event
+// function drawEvent(obj) {
+//     const trId = document.getElementById(obj.time);
+//     // if (obj.days === 'Monday') {
+//     //     trId.children[1].innerHTML = obj.name + `<span class="text-light fw-bold" style="cursor: pointer; float: right;" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})"> &times;</span>`;
+//     //     trId.children[1].classList.add('bg-success');
+//     // } else if (obj.days === 'Tuesday') {
+//     //     trId.children[2].innerHTML = obj.name + `<span class="text-light fw-bold" style="cursor: pointer; float: right;" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})"> &times;</span>`;
+//     //     trId.children[2].classList.add('bg-success');
+//     // } else if (obj.days === 'Wednesday') {
+//     //     trId.children[3].innerHTML = obj.name + `<span class="text-light fw-bold" style="cursor: pointer; float: right;" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})"> &times;</span>`;
+//     //     trId.children[3].classList.add('bg-success');
+//     // } else if (obj.days === 'Thursday') {
+//     //     trId.children[4].innerHTML = obj.name + `<span class="text-light fw-bold" style="cursor: pointer; float: right;" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})"> &times;</span>`;
+//     //     trId.children[4].classList.add('bg-success');
+//     // } else if (obj.days === 'Friday') {
+//     //     trId.children[5].innerHTML = obj.name + `<span class="text-light fw-bold" style="cursor: pointer; float: right;" data-bs-toggle="modal" data-bs-target="#modal" onClick="showModal(${obj.id})"> &times;</span>`;
+//     //     trId.children[5].classList.add('bg-success');
+//     // }
+// }
+
+// //show modal to remove event from calendar
+// function showModal(id) {
+//     let remObj = JSON.parse(localStorage.getItem(id));
+//     evName.innerHTML = '"' + remObj.name + '"';
+
+//     //remove event by clicking yes in modal
+//     remEv.addEventListener('click', function () {
+//         self.localStorage.removeItem(id);
+//         location.reload();
+//     })
+// }
+
